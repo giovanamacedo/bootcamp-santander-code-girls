647c0b7cc245d76a45b102e2ba87418df9717e41
giovanamacedo <giovanaml12@gmail.com>
2022-07-15 00:26:39 -0300

    aulas de collections - streamapi

---
+++ src/ModuloDois/Collections/StreamAPI/MainExerciciosStreamAPI.java
@@ -0,0 +1,10 @@
+package ModuloDois.Collections.StreamAPI;
+
+import  java.util.*;
+
+public class MainExerciciosStreamAPI {
+    public static void main(String[] args) {
+        List<String> numerosAleatorios =
+                Arrays.asList("1", "0", "4", "1", "2", "3", "9", "9", "6", "5");
+    }
+}
