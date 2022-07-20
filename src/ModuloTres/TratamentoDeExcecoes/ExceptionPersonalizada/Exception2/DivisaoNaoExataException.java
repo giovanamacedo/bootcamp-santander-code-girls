bb2f4818ed0d65496db89520d8d6a37f4377e345
giovanamacedo <giovanaml12@gmail.com>
2022-07-19 22:38:46 -0300

    commit exception personalizada

---
+++ src/ModuloTres/TratamentoDeExcecoes/ExceptionPersonalizada/Exception2/DivisaoNaoExataException.java
@@ -0,0 +1,15 @@
+package ModuloTres.TratamentoDeExcecoes.ExceptionPersonalizada.Exception2;
+
+public class DivisaoNaoExataException extends Exception{
+
+    private int numerador;
+    private int denominador;
+
+    public DivisaoNaoExataException(String message, int numerador, int denominador) {
+        super(message);
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+
+}
