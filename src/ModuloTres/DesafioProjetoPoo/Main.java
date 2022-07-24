a9a3dd03ed2ed3bdb4e2ff3dfc07ac7562899c0e
giovanamacedo <giovanaml12@gmail.com>
2022-07-24 17:14:47 -0300

    interligando classes de dominio

--- src/ModuloTres/DesafioProjetoPoo/Main.java
+++ src/ModuloTres/DesafioProjetoPoo/Main.java
@@ -1,8 +1,6 @@
 package ModuloTres.DesafioProjetoPoo;
 
-import ModuloTres.DesafioProjetoPoo.Dominio.Conteudo;
-import ModuloTres.DesafioProjetoPoo.Dominio.Curso;
-import ModuloTres.DesafioProjetoPoo.Dominio.Mentoria;
+import ModuloTres.DesafioProjetoPoo.Dominio.*;
 
 import java.time.LocalDate;
 
@@ -27,8 +25,32 @@ public class Main {
         Conteudo conteudo1 = new Curso();
         Conteudo conteudo2 = new Mentoria();
 
-        System.out.println(curso1);
+/*      System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria1);
+        System.out.println(mentoria1);*/
+
+        Bootcamp bootcamp1 = new Bootcamp();
+        bootcamp1.setNome("Bootcamp Java Developer");
+        bootcamp1.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp1.getConteudos().add(curso1);
+        bootcamp1.getConteudos().add(curso2);
+        bootcamp1.getConteudos().add(mentoria1);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Giovana");
+        dev1.inscreverBootcamp(bootcamp1);
+        dev1.progredir();
+        System.out.println("Conteúdos inscritos: " + dev1.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos: " + dev1.getConteudosConcluidos());
+        System.out.println("XP: " + dev1.calcularXP());
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Mateus");
+        dev2.inscreverBootcamp(bootcamp1);
+        dev2.progredir();
+        System.out.println("Conteúdos inscritos" + dev2.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos" + dev2.getConteudosConcluidos());
+        System.out.println("XP: " + dev2.calcularXP());
+
     }
 }
