9bfd6ab984d712fe487c51d9498f281c09fb852e
giovanamacedo <giovanaml12@gmail.com>
2022-07-24 16:36:48 -0300

    abstracao e encapsulamento

---
+++ src/ModuloTres/DesafioProjetoPoo/Dominio/Mentoria.java
@@ -0,0 +1,55 @@
+package ModuloTres.DesafioProjetoPoo.Dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+    private LocalDate data;
+
+    public Mentoria() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                ", data=" + data +
+                '}';
+    }
+}
---
+++ src/ModuloTres/DesafioProjetoPoo/Main.java
@@ -0,0 +1,30 @@
+package ModuloTres.DesafioProjetoPoo;
+
+import ModuloTres.DesafioProjetoPoo.Dominio.Curso;
+import ModuloTres.DesafioProjetoPoo.Dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição Mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
